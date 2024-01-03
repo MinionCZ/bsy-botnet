a3cf5e5f95b8df1fe363dc31e52cc0206b68d8ad
Jiří Štengl <stengljirka@gmail.com>
2024-01-03 16:58:07 +0100

    Upgraded slave context.py

--- slave/src/data/context.py
+++ slave/src/data/context.py
@@ -1,31 +1,37 @@
+import copy
 import threading
 import uuid
 from typing import List
 
 from common.data.commands import CommandExecutionRequest
 
-__context: "Context | None" = None
 __context_lock = threading.Lock()
 
-
-class Context:
-    def __init__(self):
-        self.bot_id: uuid.UUID = uuid.uuid4()
-        self.command_queue: List[CommandExecutionRequest] = []
+__bot_id: uuid.UUID = uuid.uuid4()
+__command_queue: List[CommandExecutionRequest] = []
+__new_commands_arrived: threading.Condition = threading.Condition()
 
 
 def add_commands_to_queue(commands: List[CommandExecutionRequest]) -> None:
     with __context_lock:
-        __context.command_queue += commands
+        global __command_queue
+        __command_queue += commands
 
 
 def get_number_of_commands_inside_queue() -> int:
     with __context_lock:
-        return len(__context.command_queue)
+        global __command_queue
+        return len(__command_queue)
 
 
 def get_commands_from_queue() -> List[CommandExecutionRequest]:
     with __context_lock:
-        commands = __context.command_queue
-        __context.command_queue = []
+        global __command_queue
+        commands = copy.deepcopy(__command_queue)
+        __command_queue = []
         return commands
+
+
+def get_new_commands_arrived_condition() -> threading.Condition:
+    global __new_commands_arrived
+    return __new_commands_arrived
